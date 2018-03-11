dd817ac23d90b296c38e329e1b6ce6cd394d3573
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2018-03-11 14:58:15 +0000

    Add Project Files

---
+++ wp-content/themes/brooklyn/app/controllers/app.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+use Sober\Controller\Controller;
+
+class App extends Controller
+{
+    public function siteName()
+    {
+        return get_bloginfo('name');
+    }
+
+    public static function title()
+    {
+        if (is_home()) {
+            if ($home = get_option('page_for_posts', true)) {
+                return get_the_title($home);
+            }
+            return __('Latest Posts', 'projectname');
+        }
+        if (is_archive()) {
+            return get_the_archive_title();
+        }
+        if (is_search()) {
+            return sprintf(__('Search Results for %s', 'projectname'), get_search_query());
+        }
+        if (is_404()) {
+            return __('Not Found', 'projectname');
+        }
+        return get_the_title();
+    }
+}
